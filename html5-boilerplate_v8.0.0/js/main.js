28e5a9785dc213ea878530b9843aef47bfe80c66
alazyfatcat <59616597+alazyfatcat@users.noreply.github.com>
2020-09-18 15:07:58 -0400

    Add files via upload

---
+++ html5-boilerplate_v8.0.0/js/main.js
@@ -0,0 +1,7 @@
+// Put custom JS here!
+// alert("Custom JS is running");
+
+function myFunction() {
+    var element = document.getElementById("show");
+    element.classList.remove("hidden");
+  }
